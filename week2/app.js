5ecfd37012f7a3ef1682cee474571a8a15cd641b
yu-niverse <chiehyu.cs09@nycu.edu.tw>
2023-10-07 00:16:07 +0800

    separated db connection code

--- week2/app.js
+++ week2/app.js
@@ -1,26 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import mysql from 'mysql2'
+import conn from './src/db/mysql/index.js'
 import router from './src/routes/users.js'
+
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT
-
-// MySQL connection
-var conn = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-});
-conn.connect(function(err) {
-  if (err) {
-    console.log('MySQL connection error');
-    return;
-  }
-  console.log('MySQL connection established');
-});
 
 // Middleware
 app.use(express.json());
--- week2/src/db/mysql/index.js
+++ week2/src/db/mysql/index.js
@@ -1,22 +1,21 @@
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-var conn = null;
-export const DB_Init = async () => {
-    try {
-        conn = await mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE
-        });
-        console.log('MySQL connection established');
-    }
-    catch (err) {
-        console.log(`${err.name}: ${err.message}`);
-        throw err;
-    }
-}
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+conn.connect((err) => {
+  if (err) {
+    console.error('MySQL connection error');
+    throw err;
+  }
+  console.log('MySQL connection established');
+});
 
 export default conn;
