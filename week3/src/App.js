c1c16002afd03b2ea45cba5ab3ba45c61ffe864d
yu-niverse <chiehyu.cs09@nycu.edu.tw>
2023-10-09 23:32:31 +0800

    added signup page frontend code

---
+++ week3/src/App.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './App.css';
+
+function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can add form validation and submit logic here
+    console.log(formData);
+  };
+
+  return (
+    <div className="App">
+      <h1>User Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={3} alignItems={'center'}>
+          <TextField 
+            type="text" 
+            id="name"
+            name="name" 
+            label="Name" 
+            variant="outlined" 
+            value={formData.name}
+            onChange={handleChange}
+            required
+            sx={{ width: '60%'}} 
+          />
+          <TextField 
+            type="email" 
+            id="email" 
+            name="email"
+            label="Email" 
+            variant="outlined" 
+            value={formData.email}
+            onChange={handleChange}
+            required
+            sx={{ width: '60%'}} 
+          />
+          <TextField 
+            type="password" 
+            id="password" 
+            name="password"
+            label="Password" 
+            variant="outlined" 
+            value={formData.password}
+            onChange={handleChange}
+            required
+            sx={{ width: '60%'}} 
+          />
+        </Stack>
+        <Button
+          variant="contained"
+          type="submit"
+          sx={{ 
+            marginTop: 5, 
+            padding: '10px 30px',
+            fontSize: '18px',
+          }}
+        > 
+          SIGN UP 
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
